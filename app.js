533cee7118b3f438725cc4cf40aca10f1a078743
PanosKontos <panos96el@gmail.com>
2022-07-31 01:13:25 +0300

    tried connceting with db

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 var express = require("express");
+// const mongoose = require("mongoose");
+// const bodyParser = require("body-parser")
+
 var app = express();
+
+
+// mongoose.connect("mongodb://localhost:27017/testdb", {
+//   useNewUrlParser: "true",
+// })
+
+// const db = mongoose.connection;
+// db.on("error", console.error.bind(console, "connection error: "));
+// db.once("open", function () {
+//   console.log("Connected successfully");
+// });
+
+
 
 app.get("/", (req, res) => {
     res.json("Hi Light friend 10");
