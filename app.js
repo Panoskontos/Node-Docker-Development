50641945dba9c4aaea376c6d9178cc6bd608ee87
Panagiotis Kontos <panagiotiskontos@adminisatorsMBP.station>
2022-07-31 16:32:27 +0300

    added db and get, create

--- app.js
+++ app.js
@@ -1,25 +1,69 @@
 var express = require("express");
-// const mongoose = require("mongoose");
-// const bodyParser = require("body-parser")
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser")
+
+// import model
+const Post = require('./models/Post')
+
 
 var app = express();
-
-
-// mongoose.connect("mongodb://localhost:27017/testdb", {
-//   useNewUrlParser: "true",
-// })
-
-// const db = mongoose.connection;
-// db.on("error", console.error.bind(console, "connection error: "));
-// db.once("open", function () {
-//   console.log("Connected successfully");
-// });
+app.use(bodyParser.json())
 
 
 
-app.get("/", (req, res) => {
-    res.json("Hi Light friend 10");
+
+// connect with db
+mongoose.connect("mongodb://localhost:27017/newdb", {
+  useNewUrlParser: "true",
+})
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error: "));
+db.once("open", function () {
+  console.log("Connected successfully");
+});
+
+
+
+
+// simple get
+app.get("/second", (req, res) => {
+    res.json("Hi second");
    });
+
+
+
+
+
+//    post request
+app.post("/", (req, res)=>{
+       const new_post = new Post({
+           title: req.body.title,
+           description: req.body.description
+        })
+        new_post.save()
+        .then(data=>{
+            res.json(data)
+        })
+        .catch(err=>{
+            res.json({ message:err})
+        })
+    })
+   
+   
+   
+// get all request
+app.get("/", async (req, res) => {
+        try{
+            const posts = await Post.find()
+            res.json(posts)
+        }catch(err){
+            res.json({message:err})
+        }
+       });
+    
+
+
 
 app.listen(3000, () => {
  console.log("Server running on port 3000");
